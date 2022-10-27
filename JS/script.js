bf49daffb31777dfccddee2031fc300f6abc05ca
Ceztio <petter.rosmo@gmail.com>
2022-10-27 19:50:43 +0200

    Adding the ability to write to the list
    Now the list works with input from the input box
    When opening the website the user gets focused on the input box
    Both the ENTER button, and the Enter key on user keybord works to add.
    
    fixed #1
    fixed #8

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,45 @@
 
 var userInput = document.getElementById("center-block");
-var theList = ["INPUT 1"]
-
-const div = document.createElement("div");
-const para = document.createElement("p");
-const node = document.createTextNode("New list element");
-div.appendChild(para);
-para.appendChild(node);
-
-const element = document.getElementById("text-list");
-element.appendChild(para);
+var theList = []
 
 
 
+//Updates the userlist from theList list. not confusing i promise
 function updateList() {
+    var list = document.getElementById("text-list");
+    list.innerHTML = "";
+    for (var i = 0; i < theList.length; i++) {
+        var item = document.createElement("li");
+        item.appendChild(document.createTextNode(theList[i]));
+        list.appendChild(item);
+    }
 
 }
 
 
+//Gets called when ENTER button is pressed
 function buttonClicked() {
-    console.log(userInput.value)
+    theList.push(userInput.value);
+    console.log(theList);
+    console.log(userInput.value);
+    updateList();
 }
+
+
+// Coppied from w3schools. it works
+var input = document.getElementById("center-block");
+
+input.addEventListener("keypress", function(event) {
+
+  if (event.key === "Enter") {
+
+    event.preventDefault();
+
+    document.getElementById("enter-button").click();
+  }
+}); 
+
+//when user enters the site, automaticly focus on input box
+window.onload = function() {
+    document.getElementById("center-block").focus();
+};
