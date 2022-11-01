e8d55e4f061cac7b1600f966e7b5eb2456db3c97
Ceztio <petter.rosmo@gmail.com>
2022-11-01 10:52:45 +0100

    Adding list counter/done counter and checkbox
    Added checkbox beside every task that is made
    When checkbox is clicked the task gets grayed out and marked as done
    Added task counter that counts how many tasks are in the list
    and how many tasks that are checked.
    
    fixed #7
    fixed #9

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,31 @@
 
 var userInput = document.getElementById("center-block");
-var theList = []
+var theList = [];
+var checkBoxCounter = 0;
+var userChecked = 0;
+var listLength = 0;
+var taskDone = 0;
 
 
+//Updates the userlist from theList list. and adds checkbox next to it
 
-//Updates the userlist from theList list. not confusing i promise
+
+// add class to the list item
 function updateList() {
     var list = document.getElementById("text-list");
-    list.innerHTML = "";
-    for (var i = 0; i < theList.length; i++) {
-        var item = document.createElement("li");
-        item.appendChild(document.createTextNode(theList[i]));
-        list.appendChild(item);
-    }
-
+    var item = document.createElement("li");
+    item.setAttribute("id", "item"+checkBoxCounter);
+    item.innerHTML = "<input type='checkbox' id='checkbox"+checkBoxCounter+"' onclick='checkboxClicked("+checkBoxCounter+")'>" + userInput.value;
+    list.appendChild(item);
+    checkBoxCounter++;
+    listLength++;
+    doneCounterFun();
 }
 
+function doneCounterFun() {
+    document.getElementById("doneCounter").innerHTML = taskDone+" / "+listLength;
+  
+}
 
 //Gets called when ENTER button is pressed
 function buttonClicked() {
@@ -23,6 +33,31 @@ function buttonClicked() {
     console.log(theList);
     console.log(userInput.value);
     updateList();
+}
+
+
+function checkboxClicked(number) {
+  var item = document.getElementById("item"+number);
+    if (document.getElementById("checkbox"+number).checked) {
+      item.style.textDecoration = "line-through";
+      item.style.color = "grey";
+      item.style.fontStyle = "italic";
+      userChecked++;
+      taskDone++;
+    }
+    else {
+      item.style.textDecoration = "none";
+      item.style.color = "white";
+      item.style.fontStyle = "normal";
+      userChecked--;
+      taskDone--;
+    }
+    doneCounterFun();
+    console.log(userChecked)
+    // var itemChange = document.getElementById("item"+number);
+    // console.log("ItemChangeValue = "+itemChange.innerText)
+    // itemChange.id = "lineOverText";
+  
 }
 
 
@@ -38,6 +73,8 @@ input.addEventListener("keypress", function(event) {
     document.getElementById("enter-button").click();
   }
 }); 
+
+
 
 //when user enters the site, automaticly focus on input box
 window.onload = function() {
