7133b70eda0af00be6b0f59ed5156067ac672c7c
Ceztio <petter.rosmo@gmail.com>
2022-11-22 11:08:21 +0100

    Adding cookies (not working)
    The websites remembers the list when refreshing
    Delete button that deletes the entiere list
    
    #2
    #10

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,33 @@
 
 var userInput = document.getElementById("center-block");
-var theList = [];
+var theList = JSON.parse(localStorage.getItem("items"));
+var checkboxClickedArray = JSON.parse(localStorage.getItem("check"));
 var checkBoxCounter = 0;
 var userChecked = 0;
 var listLength = 0;
 var taskDone = 0;
 
 
-//Updates the userlist from theList list. and adds checkbox next to it
+if (!Array.isArray(checkboxClickedArray)){
+  checkboxClickedArray = []
+}
+
+if (!Array.isArray(theList)){
+  theList = []
+}
+
+if (theList != []){
+    for (var i = 0; i < theList.length; i++) {
+        updateList();
+    }
+}
+
+if (checkboxClickedArray != []){
+    for (var i = 0; i < checkboxClickedArray; i++) {
+        checkboxAutoClicked(checkboxClickedArray[i]);
+    }
+}
+
 
 
 // add class to the list item
@@ -19,8 +39,36 @@ function updateList() {
     list.appendChild(item);
     checkBoxCounter++;
     listLength++;
+    console.log(checkboxClickedArray)
     doneCounterFun();
+    localCookie()
 }
+
+
+
+
+function deleteAll() {
+  if (confirm("Are you sure you want to delete the entire list?")){
+
+  var list = document.getElementById("text-list");
+  list.innerHTML = "";
+  checkBoxCounter = 0;
+  listLength = 0;
+  theList = [];
+  checkboxClickedArray = [];
+  localStorage.setItem("check", JSON.stringify(theList)); // saves state of checkbox clicks
+  doneCounterFun();
+  localCookie()
+  }
+
+}
+
+
+function localCookie(){
+  localStorage.setItem("items", JSON.stringify(theList));
+}
+
+
 
 function doneCounterFun() {
     document.getElementById("doneCounter").innerHTML = taskDone+" / "+listLength;
@@ -44,6 +92,7 @@ function checkboxClicked(number) {
       item.style.fontStyle = "italic";
       userChecked++;
       taskDone++;
+      checkboxClickedArray.push(number)
     }
     else {
       item.style.textDecoration = "none";
@@ -52,11 +101,39 @@ function checkboxClicked(number) {
       userChecked--;
       taskDone--;
     }
+
+
+
+    // change array value to true or false in the same position as the checkbox
+    
+    localStorage.setItem("check", JSON.stringify(checkboxClickedArray)); // saves state of checkbox clicks
     doneCounterFun();
     console.log(userChecked)
-    // var itemChange = document.getElementById("item"+number);
-    // console.log("ItemChangeValue = "+itemChange.innerText)
-    // itemChange.id = "lineOverText";
+    console.log("ID clicked: " +  number)
+  
+}
+
+
+
+
+function checkboxAutoClicked(number) {
+  var item = document.getElementById("item"+number);
+
+  item.style.textDecoration = "line-through";
+  item.style.color = "grey";
+  item.style.fontStyle = "italic";
+  userChecked++;
+  taskDone++;
+  checkboxClickedArray.push(number)
+    
+
+
+
+    // change array value to true or false in the same position as the checkbox
+    
+    doneCounterFun();
+    console.log(userChecked)
+    console.log("ID clicked: " +  number)
   
 }
 
